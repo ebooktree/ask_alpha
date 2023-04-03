64ad00239d39ec0a8dc68d8f843ff50b8cf504e7
haemin kim <haeminkim@haemins-MacBook-Air.local>
2023-04-03 21:32:09 +0900

    error 404, 500

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flaskext.markdown import Markdown
@@ -17,6 +17,11 @@ naming_convention = {
 db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
 
+# 오류 페이지 추가 404
+def page_not_found(e):
+    return render_template('404.html'), 404
+def server_error(e):
+    return render_template('500.html'), 500
 
 def create_app():
     app = Flask(__name__)
@@ -44,5 +49,9 @@ def create_app():
 
     # markdown
     Markdown(app, extensions=['nl2br', 'fenced_code'])
+
+    # 오류 페이지 404 추가
+    app.register_error_handler(404, page_not_found)
+    app.register_error_handler(500, server_error)
 
     return app
