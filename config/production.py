6237ce2ec54e9d938f4e2f81a32ca18b2a149bf8
haemin kim <haeminkim@haemins-MacBook-Air.local>
2023-04-03 21:47:13 +0900

    flask logging

--- config/production.py
+++ config/production.py
@@ -1,7 +1,31 @@
 # 보통 서버 환경을 production 환경이라고 하므로 파일 이름을 server가 아니라 production으로 지었다.
+from logging.config import dictConfig
 
 from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'*!\xda\xd0\r\xcb\xa8\x06\xe3\xc9\xb4[\x97x\xca\x9b'
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
